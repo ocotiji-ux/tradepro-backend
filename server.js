34a40eb98bdf06d4d1b1a3cf33ed3863c3f1b717
Okenna Otiji <ocotiji@gmail.com>
2026-05-03 15:53:10 +0100

    add server

--- server.js
+++ server.js
@@ -26,3 +26,26 @@ wss.on("connection", (ws) => {
 })
 
 console.log("WebSocket running on port 8080")
+const http = require("http");
+const WebSocket = require("ws");
+
+const server = http.createServer();
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  setInterval(() => {
+    ws.send(JSON.stringify({
+      pair: "EURUSD",
+      price: (1 + Math.random()).toFixed(5)
+    }));
+  }, 1000);
+});
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
