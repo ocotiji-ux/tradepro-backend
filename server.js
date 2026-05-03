9e2f6a6ef95993d2e1025ae79782a4fa8903b6bc
Okenna Otiji <ocotiji@gmail.com>
2026-05-03 16:34:58 +0100

    fix websocket duplicate declaration

--- server.js
+++ server.js
@@ -1,31 +1,3 @@
-const WebSocket = require("ws")
-
-const PORT = 8080
-const wss = new WebSocket.Server({ port: PORT })
-
-const PAIRS = ["EURUSD", "GBPUSD", "USDJPY"]
-
-function generatePrice(pair) {
-  return {
-    pair,
-    bid: (Math.random() + 1).toFixed(5),
-    ask: (Math.random() + 1.002).toFixed(5),
-    time: new Date()
-  }
-}
-
-wss.on("connection", (ws) => {
-  console.log("Client connected")
-
-  const interval = setInterval(() => {
-    const data = PAIRS.map(generatePrice)
-    ws.send(JSON.stringify(data))
-  }, 1000)
-
-  ws.on("close", () => clearInterval(interval))
-})
-
-console.log("WebSocket running on port 8080")
 const http = require("http");
 const WebSocket = require("ws");
 
@@ -33,19 +5,32 @@ const server = http.createServer();
 
 const wss = new WebSocket.Server({ server });
 
+const PAIRS = ["EURUSD", "GBPUSD", "USDJPY"];
+
+function generatePrice(pair) {
+  return {
+    pair,
+    bid: (Math.random() + 1).toFixed(5),
+    ask: (Math.random() + 1.01).toFixed(5),
+    time: new Date(),
+  };
+}
+
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
-  setInterval(() => {
-    ws.send(JSON.stringify({
-      pair: "EURUSD",
-      price: (1 + Math.random()).toFixed(5)
-    }));
+  const interval = setInterval(() => {
+    const data = PAIRS.map(generatePrice);
+    ws.send(JSON.stringify(data));
   }, 1000);
+
+  ws.on("close", () => {
+    clearInterval(interval);
+  });
 });
 
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
